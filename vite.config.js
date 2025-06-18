3c6ad4f87509caa8ba4b20500f5fe64d4f413631
dev-gicbo <dev.logicboolean@gmail.com>
2025-06-18 20:20:11 +0700

    start tahap 2 react breeze. masih error.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.jsx',
             refresh: true,
         }),
-        tailwindcss(),
+        react(),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        cors: true,
+        hmr: {
+            host: '192.168.1.4', // Ganti dengan IP LAN Anda
+        },
+    },
 });
